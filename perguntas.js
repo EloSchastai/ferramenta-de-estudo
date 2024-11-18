8c68176b8f7b20f63be66199bd7b165c191e5e74
EloSchastai <160937316+EloSchastai@users.noreply.github.com>
2024-11-18 14:04:38 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Química',
+    'Quais são os ramos que a Química se divide?',
+    'química orgânica, química inorgânica, físico-química e química analítica'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital do Paraná?',
+    'A capital de Curitiba'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que é um algoritmo?',
+    'Um algoritmo é um conjunto das regras e procedimentos lógicos perfeitamente definidos que levam à solução de um problema em um número finito de etapas.'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz computador em Inglês?',
+    'Computador em ingles é computer'
 )
